f71da3ad6963976332e9c90bd38010db1e7e60bd
raneasaad01 <152267087+raneasaad01@users.noreply.github.com>
2023-11-30 13:04:47 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Sierra Leone'
+copyright = '2023, Ranea Saad'
+author = 'Ranea Saad'
 
 release = '0.1'
 version = '0.1.0'
